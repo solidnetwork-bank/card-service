2c71b85f4006aa5d86714b917f5c1a826e5d81b0
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-27 14:05:44 +0000

    adds /detail GET and POST methods.

--- src/main/java/xyz/solidnetwork/service/CardController.java
+++ src/main/java/xyz/solidnetwork/service/CardController.java
@@ -2,11 +2,17 @@ package xyz.solidnetwork.service;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class CardController {
 	@PostMapping("/detail")
-	public Card getDetail() {
+	public Card getPostDetail() {
+		return new Card();
+	}
+
+	@GetMapping("/detail")
+	public Card getGetDetail() {
 		return new Card();
 	}
 }

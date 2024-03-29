5f68836a857b44d8e9d1012ee2cb417ae722dff4
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-29 00:41:40 +0000

    adds lombok @Slf4j logger.

--- src/main/java/xyz/solidnetwork/service/CardController.java
+++ src/main/java/xyz/solidnetwork/service/CardController.java
@@ -1,26 +1,26 @@
 package xyz.solidnetwork.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class CardController {
-	private static final Logger logger = LoggerFactory.getLogger(CardController.class);
 
 	@PostMapping("/detail")
 	public Card getPostDetail() {
 		Card cardDetail = new Card();
-		logger.trace("card details POST request ={}", cardDetail);
+		log.info("card details POST request = {}", cardDetail);
 		return cardDetail;
 	}
 
 	@GetMapping("/detail")
 	public Card getGetDetail() {
 		Card cardDetail = new Card();
-		logger.trace("card details GET request ={}", cardDetail);
+		log.info("card details GET request = {}", cardDetail);
 		return cardDetail;
 	}
 }

0f0bf3e427fd155cb90cab13b2a17fee4323bb3f
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 19:24:02 -0400

    adds timestamp in before-install.sh CodeDeploy

--- .codedeploy/scripts/before-install.sh
+++ .codedeploy/scripts/before-install.sh
@@ -5,12 +5,11 @@ echo "-------------------------------------------------------" >> /opt/codedeplo
 
 # Check whether the jar exists or not
 cd /opt/solidnetwork/bank/services
-pwd >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
-ls >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+pwd | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [pwd] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+ls | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [ls] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 if [ -f 'card-service.jar' ]
 then
-    sudo rm card-service.jar
-    echo "card-service.jar is removed" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+    sudo rm -v -f card-service.jar | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [rm] $_"' | sudo tee -a /opt/codedeploy-agent/logs/codedeploy-deployments.log
 fi
 
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log

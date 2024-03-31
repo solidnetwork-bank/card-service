2df76eeec83b27f57f3e14b006d7163cde8e6f45
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 17:02:34 -0400

    adds dir and ls to before-install scrip CodeDeploy logs

--- .codedeploy/scripts/before-install.sh
+++ .codedeploy/scripts/before-install.sh
@@ -10,6 +10,11 @@ fi
 echo " B E F O R E  I N S T A L L                            " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
+#  make sure the four shell scripts have execute permissions
+pwd >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+ls >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+sudo chmod -v +x ./*.sh | sudo tee -a /opt/codedeploy-agent/logs/codedeploy-deployments.log
+
 cd /opt/solidnetwork/bank/services
 # Check whether the file exists or not
 if [ -f 'card-service.jar' ]
@@ -17,8 +22,6 @@ then
     sudo rm card-service.jar
     echo "card-service.jar is removed" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 fi
-
-sudo chmod -v +x ./*.sh | sudo tee -a /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo " BEFORE INSTALL SUCCESS                                " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log

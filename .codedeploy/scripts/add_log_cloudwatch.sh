9240f6549b094465a86862339deea7ebe5216075
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 08:55:01 -0400

    updates CloudWatch config

--- .codedeploy/scripts/add_log_cloudwatch.sh
+++ .codedeploy/scripts/add_log_cloudwatch.sh
@@ -1,5 +1,5 @@
-cd /opt/aws/amazon-cloudwatch-agent/etc/
+cd /opt/aws/amazon-cloudwatch-agent/etc
 rm -f amazon-cloudwatch-agent.json
-wget https://raw.githubusercontent.com/solidnetwork-bank/IaC/main/AWS/CloudWatch/amazon-cloudwatch-agent-Everglades.json
-mv amazon-cloudwatch-agent-Everglades.json amazon-cloudwatch-agent.json
+wget https://raw.githubusercontent.com/solidnetwork-bank/IaC/main/AWS/CloudWatch/amazon-cloudwatch-agent-EC2-Everglades.json
+mv amazon-cloudwatch-agent-EC2-Everglades.json amazon-cloudwatch-agent.json
 amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json

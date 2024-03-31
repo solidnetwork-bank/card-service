331221d2291595c60a1650ec897c56a7d45044b5
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-30 21:35:49 -0400

    adds cloudwatch script to CodeDeploy appspec.yml to add log to the agent each deploy

---
+++ .codedeploy/scripts/add_log_cloudwatch.sh
@@ -0,0 +1,5 @@
+cd /opt/aws/amazon-cloudwatch-agent/etc/
+rm -f amazon-cloudwatch-agent.json
+wget https://raw.githubusercontent.com/solidnetwork-bank/IaC/main/AWS/CloudWatch/amazon-cloudwatch-agent-Everglades.json
+mv amazon-cloudwatch-agent-Everglades.json amazon-cloudwatch-agent.json
+amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json

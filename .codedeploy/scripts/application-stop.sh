5b42909347d066b80122461b69c6a94aa55d55a7
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 16:47:44 -0400

    updates application-stop codedeploy script

--- .codedeploy/scripts/application-stop.sh
+++ .codedeploy/scripts/application-stop.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+cd /opt/codedeploy-agent/logs
+# Check whether the log exists or not
+if [ -f 'codedeploy-deployments.log' ]
+then
+    echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+else
+echo "-------------------------------------------------------" > /opt/codedeploy-agent/logs/codedeploy-deployments.log
+fi
+
+echo " A P P L I C A T I O N  S T O P                        " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
 
 #sudo killall java
@@ -12,4 +23,6 @@
 
 #pkill -f /var/appname.jar
 
-#echo "The ApplicationStop deployment lifecycle event successfully completed." > /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo " APPLICATION STOP SUCCESS                                " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log

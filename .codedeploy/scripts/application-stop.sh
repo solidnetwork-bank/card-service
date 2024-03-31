e66f76cf58a7cba10567b0dd94f4ecebc5ea8042
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 19:29:04 -0400

    updates application-stop script to add pwd CodeDeploy

--- .codedeploy/scripts/application-stop.sh
+++ .codedeploy/scripts/application-stop.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-cd /opt/codedeploy-agent/logs
-# Check whether the log exists or not
-if [ -f 'codedeploy-deployments.log' ]
-then
-    echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
-else
-echo "-------------------------------------------------------" > /opt/codedeploy-agent/logs/codedeploy-deployments.log
-fi
-
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo " A P P L I C A T I O N  S T O P                        " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
+#  make sure the four shell scripts have execute permissions
+pwd | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [pwd] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+ls | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [ls] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+#sudo chmod -v +x ./*.sh | sudo tee -a /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
 #sudo killall java
 

fe05c188c4ea4d500a00d988ea5d169a0bc39585
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 21:04:48 -0400

    merge ApplicationStop and BeforeInstall CodeDeploy

--- .codedeploy/scripts/before-install.sh
+++ .codedeploy/scripts/before-install.sh
@@ -3,7 +3,17 @@ echo "-------------------------------------------------------" >> /opt/codedeplo
 echo " B E F O R E  I N S T A L L                            " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
-# Check whether the jar exists or not
+# terminate java process
+process=`ps -ef | grep -v awk | awk -e '/java.*card-service.jar/ { print $2 }'`
+if [ -z "$process" ]
+then
+    echo "no process to terminate" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+else
+      echo "terminating process ${process}" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+      sudo kill -TERM ${process} | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [kill] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+fi
+
+# remove jar file
 cd /opt/solidnetwork/bank/services
 pwd | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [pwd] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 ls | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [ls] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log

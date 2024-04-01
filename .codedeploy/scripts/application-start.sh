52db1b8b511a19d6afc5abe456d8b4b11f98936b
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 21:19:46 -0400

    merge AfterInstall & ApplicationStart CodeDeploy scripts

--- .codedeploy/scripts/application-start.sh
+++ .codedeploy/scripts/application-start.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo " A P P L I C A T I O N  S T A R T                      " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+
+# run service in port=80
+cd /opt/solidnetwork/bank/services
+pwd | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [pwd] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+ls | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [ls] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+if [ -f 'card-service.jar' ]
+then
+    sudo java -jar -Dserver.port=80 card-service.jar > /dev/null 2> /dev/null < /dev/null &
+    echo "service is deployed, check log" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+else
+   echo "no service to deploy with that name" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+fi
+
+# add log to CloudWatch Agent
+#cd /opt/aws/amazon-cloudwatch-agent/etc
+#sudo rm -f amazon-cloudwatch-agent.json
+#sudo wget https://raw.githubusercontent.com/solidnetwork-bank/IaC/main/AWS/CloudWatch/amazon-cloudwatch-agent-EC2-Everglades.json
+#sudo mv amazon-cloudwatch-agent-EC2-Everglades.json amazon-cloudwatch-agent.json
+#sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
+
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo " APPLICATION START SUCCESS                                " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+
+
+
 
  ## **** /tmp/sleep.sh > /dev/null 2> /dev/null < /dev/null &
 #cd /opt/solidnetwork/bank/services
@@ -22,3 +52,9 @@
 
 
 #echo "The ApplicationStart deployment lifecycle event successfully completed." > /opt/codedeploy-agent/logs/codedeploy-deployments.log
+
+
+
+
+
+#echo "The AfterInstall deployment lifecycle event successfully completed." > /opt/codedeploy-agent/logs/codedeploy-deployments.log

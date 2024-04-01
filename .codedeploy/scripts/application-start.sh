28d36b09192bd341c8067aa9fbf6c7d02e24c78a
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-04-01 13:27:24 -0400

    updates CodeDeploy validate-service.sh

--- .codedeploy/scripts/application-start.sh
+++ .codedeploy/scripts/application-start.sh
@@ -15,46 +15,6 @@ else
    echo "no service to deploy with that name" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 fi
 
-# add log to CloudWatch Agent
-#cd /opt/aws/amazon-cloudwatch-agent/etc
-#sudo rm -f amazon-cloudwatch-agent.json
-#sudo wget https://raw.githubusercontent.com/solidnetwork-bank/IaC/main/AWS/CloudWatch/amazon-cloudwatch-agent-EC2-Everglades.json
-#sudo mv amazon-cloudwatch-agent-EC2-Everglades.json amazon-cloudwatch-agent.json
-#sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-
-
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
-echo " APPLICATION START SUCCESS                                " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo " APPLICATION START SUCCESS                             " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
-
-
-
-
- ## **** /tmp/sleep.sh > /dev/null 2> /dev/null < /dev/null &
-#cd /opt/solidnetwork/bank/services
-#sudo java -jar -Dserver.port=80 \
-#    *.jar > /dev/null 2> /dev/null < /dev/null &
-
-#nohup java -jar -Dserver.port=80 card-service.jar &
-
-
-#java -jar card-service.jar > /opt/solidnetwork/bank/logs/card-service.log 2>&1 &
-
-#nohup java -jar card-service.jar > /dev/null &
-
-
-#sudo nohup java -jar -Dserver.port=80 card-service.jar > /dev/null 2> /dev/null < /dev/null &
-
-
-#sudo wget https://github.com/solidnetwork-bank/card-service/blame/5b752f67097c33602cecc2d80b24192c4393a22e/target/service-0.0.2-SNAPSHOT.jar
-#sudo mv service-0.0.2-SNAPSHOT.jar card-service.jar
-#java - jar card-service.jar
-
-
-#echo "The ApplicationStart deployment lifecycle event successfully completed." > /opt/codedeploy-agent/logs/codedeploy-deployments.log
-
-
-
-
-
-#echo "The AfterInstall deployment lifecycle event successfully completed." > /opt/codedeploy-agent/logs/codedeploy-deployments.log
--- .codedeploy/scripts/validate-service.sh
+++ .codedeploy/scripts/validate-service.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo " V A L I D A T E  S E R V I C E                        " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
+while [ true ]
+  do
+    if [ "$(curl -s http://localhost:80/actuator/health)" = '{"status":"UP"}' ]
+     then
+       echo "/actuator/health STATUS=UP" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+       exit 0
+    else
+       echo "/actuator/health STATUS is not UP or it can't be consulted" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+       sleep 3s
+    fi
+done
 
-#while [ true ]
-#do
-#    if [ "$(curl -s http://localhost:80/actuator/health)" = '{"status":"UP"}' ]
-#    then
-#        exit 0
-#    else
-##        echo "check server is running?"
- #       sleep 3s
- #   fi
-#done
-
-#echo "The ValidateService deployment lifecycle event successfully completed." > /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo " VALIDATE SERVICE SUCCESS                              " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log

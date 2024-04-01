48ada4fb17f852f0ff5b322c077a227fa17ce8c6
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-31 20:44:23 -0400

    updates application-stop CodeDeploy script

--- .codedeploy/scripts/application-stop.sh
+++ .codedeploy/scripts/application-stop.sh
@@ -3,21 +3,15 @@ echo "-------------------------------------------------------" >> /opt/codedeplo
 echo " A P P L I C A T I O N  S T O P                        " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 
-#  make sure the four shell scripts have execute permissions
-pwd | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [pwd] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
-ls | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [ls] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
-#sudo chmod -v +x ./*.sh | sudo tee -a /opt/codedeploy-agent/logs/codedeploy-deployments.log
-
-#sudo killall java
-
-#process=`ps -ef | grep -v awk | awk -e '/java.*<PROCESS_NAME>/ { print $2 }'`
-#kill ${process}
-
-#process=`ps -ef | grep -v awk | awk -e '/java.*'"${PROCESS_NAME}"'/ { print $2 }'`
-#kill -TERM ${process}
-
-
-#pkill -f /var/appname.jar
+# terminate java process
+process=`ps -ef | grep -v awk | awk -e '/java.*card-service.jar/ { print $2 }'`
+if [ -z "$process" ]
+then
+    echo "no process to terminate" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+else
+      echo "terminating process ${process}" | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [echo] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+      sudo kill -TERM ${process} | perl -MPOSIX -ne 'BEGIN{ $|=1 } print strftime( "%Y-%m-%d %H:%M:%S", localtime ) . " [kill] $_"' >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
+fi
 
 echo "-------------------------------------------------------" >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
 echo " APPLICATION STOP SUCCESS                                " >> /opt/codedeploy-agent/logs/codedeploy-deployments.log
